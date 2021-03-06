a8043fa375fbc1d1ef39f5135b12ed24e19d4b49
Rishi Kothari <itsrishikothari@gmail.com>
2021-03-06 08:37:52 -0500

    user module

---
+++ src/user/user.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { User } from './user'
+import { UserService } from './service/user.service'
+import { UserResolver } from './resolver/user.resolver'
+import { Pet } from '../pet/pet'
+import { Vaccination } from '../vaccination/vaccination'
+import { Reminder } from '../reminder/reminder'
+import { Scan } from '../scan/scan'
+import { Event } from '../event/event'
+import { Contact } from '../contact/contact'
+
+@Module({
+	exports: [UserService],
+	providers: [UserService, UserResolver],
+	imports: [
+		TypeOrmModule.forFeature([
+			User,
+			Pet,
+			Vaccination,
+			Reminder,
+			Scan,
+			Event,
+			Contact,
+		]),
+	],
+})
+export class UserModule {}
