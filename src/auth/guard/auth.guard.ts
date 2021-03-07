473704e04449301091c14c75c657fcece05309b7
Rishi Kothari <itsrishikothari@gmail.com>
2021-03-07 07:28:41 -0500

    auth guard

---
+++ src/auth/guard/auth.guard.ts
@@ -0,0 +1,36 @@
+import {
+	CanActivate,
+	ExecutionContext,
+	Inject,
+	Injectable,
+} from '@nestjs/common'
+import { Observable } from 'rxjs'
+import { GqlContextType, GqlExecutionContext } from '@nestjs/graphql'
+import { FirebaseAuthenticationService } from '@aginix/nestjs-firebase-admin'
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+	constructor(
+		@Inject('FirebaseAuthenticationService')
+		private readonly auth: FirebaseAuthenticationService
+	) {}
+	async canActivate(context: ExecutionContext): Promise<boolean> {
+		if (context.getType<GqlContextType>() === 'graphql') {
+			const ctx = GqlExecutionContext.create(context)
+			console.log(ctx.getContext().req.get('token'))
+			try {
+				const z = await this.auth.verifyIdToken(
+					ctx.getContext().req.get('token')
+				)
+
+				if (z.uid) {
+					return true
+				}
+			} catch (e) {
+				throw new Error(e)
+			}
+		}
+
+		return false
+	}
+}
