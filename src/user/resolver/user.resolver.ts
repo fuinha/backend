f25f4c7309ac3d864963611dd9b1c9f8a617cb5b
Rishi Kothari <itsrishikothari@gmail.com>
2021-03-06 21:24:40 -0500

    user field

--- src/user/resolver/user.resolver.ts
+++ src/user/resolver/user.resolver.ts
@@ -1,12 +1,45 @@
-import { Args, Mutation, Resolver } from '@nestjs/graphql'
+import { Args, Mutation, ResolveField, Resolver, Root } from '@nestjs/graphql'
 import { User } from '../user'
 import { UserService } from '../service/user.service'
+import { BaseEntityResolver } from '../../shared/graphql'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Pet } from '../../pet/pet'
+import { Repository } from 'typeorm'
 
 @Resolver()
-export class UserResolver {
-	constructor(private userService: UserService) {}
+export class UserResolver extends BaseEntityResolver(User) {
+	constructor(private userService: UserService) {
+		super(userService)
+	}
+
 	@Mutation(() => User)
 	createUser(@Args('uid') uid: string) {
 		return this.userService.createUser(uid)
+	}
+}
+
+@Resolver(() => User)
+export class UserFieldResolver {
+	constructor(
+		@InjectRepository(User) private repo: Repository<User>,
+		@InjectRepository(Pet) private pet: Repository<Pet>
+	) {}
+
+	@ResolveField(() => [Pet])
+	public async caretakingPets(@Root() root: User) {
+		const user = await this.repo.findOneOrFail(root.id, {
+			relations: ['caretakingPets'],
+		})
+
+		return user.caretakingPets
+	}
+
+	@ResolveField(() => [Pet])
+	public async ownedPets(@Root() root: User) {
+		const user = await this.repo.findOneOrFail(root.id, {
+			relations: ['ownedPets'],
+		})
+
+		return user.ownedPets
 	}
 }
